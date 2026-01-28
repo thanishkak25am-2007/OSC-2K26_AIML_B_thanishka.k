85229aaf1a02bc80cadf1e26c56fdb5d756f1cfa
thanishkak25am-2007 <thanishka.k_25am@kgkite.ac.in>
2026-01-28 09:18:24 +0530

    promblem_066 solved

--- problems/medium/problem_066.py
+++ problems/medium/problem_066.py
@@ -6,7 +6,7 @@ Difficulty: Medium
 
 def convert_temperature(value):
     factor = 0.5
-    return value // factor
-    return value + factor
+    return value * factor
+
 
 print(convert_temperature(100))
