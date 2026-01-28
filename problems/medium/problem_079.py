9625c807a963f7ed4ad280f8dcb3636da8d9fd61
thanishkak25am-2007 <thanishka.k_25am@kgkite.ac.in>
2026-01-28 09:59:22 +0530

    promblem_079 solved

--- problems/medium/problem_079.py
+++ problems/medium/problem_079.py
@@ -8,7 +8,7 @@ def calculate_distance(items):
     total = 0
     # Or pure logic error like total = items[0] then loop skips 0
     for item in items:
-        total = item # Overwrites total instead of adding
+        total += item # Overwrites total instead of adding
     return total
 
 print(calculate_distance([10, 20, 30]))
