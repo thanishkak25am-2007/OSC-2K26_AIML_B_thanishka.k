563e0cb438190d0d43a7739d5d6707b105dfbc3d
thanishkak25am-2007 <thanishka.k_25am@kgkite.ac.in>
2026-01-28 10:00:30 +0530

    promblem_080 solved

--- problems/medium/problem_080.py
+++ problems/medium/problem_080.py
@@ -6,7 +6,7 @@ Difficulty: Medium
 
 def convert_distance(value):
     factor = 0.5
-    return value // factor
-    return value + factor
+    return value * factor
+    
 
 print(convert_distance(100))
