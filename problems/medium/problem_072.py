db0919c0a122b155eaa1dc7225af47a076b3f5f9
thanishkak25am-2007 <thanishka.k_25am@kgkite.ac.in>
2026-01-28 09:28:32 +0530

    promblem_072 solved

--- problems/medium/problem_072.py
+++ problems/medium/problem_072.py
@@ -8,7 +8,7 @@ def calculate_currency(items):
     total = 0
     # Or pure logic error like total = items[0] then loop skips 0
     for item in items:
-        total = item # Overwrites total instead of adding
+        total += item # Overwrites total instead of adding
     return total
 
 print(calculate_currency([10, 20, 30]))
