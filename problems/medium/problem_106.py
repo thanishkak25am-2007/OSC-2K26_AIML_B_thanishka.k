a4ff5b7ea0c7f6b8d26652c84f6e800e8fac62db
thanishkak25am-2007 <thanishka.k_25am@kgkite.ac.in>
2026-01-29 14:15:03 +0530

    promblem_106 solved

--- problems/medium/problem_106.py
+++ problems/medium/problem_106.py
@@ -9,8 +9,9 @@ def check_calories(val):
 
     if val > 10:
         return 'High'
-    if val > 5:
+    elif val > 5:
         return 'Medium'
-    return 'Low' 
+    else:
+        return 'Low' 
     
 print(check_calories(20))
