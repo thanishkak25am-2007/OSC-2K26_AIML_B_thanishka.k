2d954f2b69256f5bfd1d80207ae061440a1e2a33
thanishkak25am-2007 <thanishka.k_25am@kgkite.ac.in>
2026-01-29 14:26:15 +0530

    promblem_114 solved

--- problems/medium/problem_114.py
+++ problems/medium/problem_114.py
@@ -8,7 +8,7 @@ def calculate_speed(items):
     total = 0
     # Or pure logic error like total = items[0] then loop skips 0
     for item in items:
-        total = item # Overwrites total instead of adding
+        total += item # Overwrites total instead of adding
     return total
 
 print(calculate_speed([10, 20, 30]))
