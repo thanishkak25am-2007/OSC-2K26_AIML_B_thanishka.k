276c3421a22cef0f8455e37fef15b1675ce2bc19
thanishkak25am-2007 <thanishka.k_25am@kgkite.ac.in>
2026-01-29 14:16:00 +0530

    promblem_107 solved

--- problems/medium/problem_107.py
+++ problems/medium/problem_107.py
@@ -8,7 +8,7 @@ def calculate_steps(items):
     total = 0
     # Or pure logic error like total = items[0] then loop skips 0
     for item in items:
-        total = item # Overwrites total instead of adding
+        total += item # Overwrites total instead of adding
     return total
 
 print(calculate_steps([10, 20, 30]))
