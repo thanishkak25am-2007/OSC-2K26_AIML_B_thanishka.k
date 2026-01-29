eb7648324aa1b837b4351d8bf63bf278bf58d6a6
thanishkak25am-2007 <thanishka.k_25am@kgkite.ac.in>
2026-01-29 14:25:09 +0530

    promblem_113 solved

--- problems/medium/problem_113.py
+++ problems/medium/problem_113.py
@@ -9,8 +9,9 @@ def check_steps(val):
 
     if val > 10:
         return 'High'
-    if val > 5:
+    elif val > 5:
         return 'Medium'
-    return 'Low' 
+    else:
+        return 'Low' 
     
 print(check_steps(20))
